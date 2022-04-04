a4b932ac987ae397f0fd45cce943158516dff78c
chryslimjy <75465315+chryslimjy@users.noreply.github.com>
2022-04-04 14:34:30 +0800

    Week 10 lab

---
+++ input.py
@@ -0,0 +1,6 @@
+#finds the average of 2 user input values
+from tokenize import Double
+x = Double(input("Enter value for x: "))
+y = Double(input("Enter value for y: "))
+avg = (x+y)/2
+print("Avg is: %0.2f"  %avg)
---
+++ q3.py
@@ -0,0 +1,29 @@
+#switch case in python
+module = "1009"
+match module: 
+    case "1010":
+        print("Computer Networks")
+      
+    case "1006":
+        print("Mathematics 2")
+   
+    case "1009":
+        print("Object Oriented Programming")
+      
+    case "2902":
+        print("Career and Professional Development")
+   
+    case "1007":
+        print("Operating Systems")
+
+
+#prints out odd numbers from 66 to 102 in descending order
+for x in range(102, 66, -1): 
+      if (x % 2 == 1): 
+        print(x)
+      
+
+    
+
+     
+    
